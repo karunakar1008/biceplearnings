b25c7b1d36dc8ec2c4fc246d3991c07d751d1fcf
Bhogyari Karunakar <kbhogyari@@deloitte.com>
2025-12-05 11:44:02 +0530

    publish web app to staging slot

--- powershelllearnings/5. Lab - Azure PowerShell - Deployment from Github/script03.ps1
+++ powershelllearnings/5. Lab - Azure PowerShell - Deployment from Github/script03.ps1
@@ -14,22 +14,19 @@ New-AzWebApp -ResourceGroupName $ResourceGroupName -Location $Location -Name $Ap
 $GitRepoUrl="https://github.com/karunakar1008/BlazorBlackBelt.git"
 $GitBranch="main"
 
-Set-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppName `
--SourceControlRepositoryUrl $GitRepoUrl -SourceControlBranch $GitBranch -SourceControlType GitHub
-
-# Restart the web app to apply changes
-Restart-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppName
-Write-Output "Web App deployed successfully. Access it at: https://$AppName.azurewebsites.net"
-
-
-$gitrepo="https://github.com/cloudlearning4000/learningapp"
-
 $PropertiesObject = @{
-    repoUrl = $gitrepo;
-    branch ="main";
+    repoUrl = $GitRepoUrl;
+    branch =$GitBranch;
     isManualIntegration = "true";
 }
 
 Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName $ResourceGroupName `
 -ResourceType Microsoft.Web/sites/sourcecontrols `
 -ResourceName $AppName/web -ApiVersion 2015-08-01 -Force
+
+# Restart the web app to apply changes
+Restart-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppName
+Write-Output "Web App deployed successfully. Access it at: https://$AppName.azurewebsites.net"
+
+# Output the URL of the deployed web app
+Write-Output "Web App URL: https://$AppName.azurewebsites.net"
---
+++ powershelllearnings/6. Lab - Azure PowerShell - Deployment Slots/script04.ps1
@@ -0,0 +1,35 @@
+$Location="North Europe"
+$ResourceGroupName="web-grp"
+$AppServicePlanName="learningplan7000"
+$AppName="leaningapp588787"
+$SlotName="staging"
+
+#Sclae up the App Service Plan to Standard tier to enable deployment slots
+Set-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlanName -Tier "Standard" 
+
+# Create a deployment slot
+New-AzWebAppSlot -ResourceGroupName $ResourceGroupName -Name $AppName -Slot $SlotName -AppServicePlan $AppServicePlanName
+
+# Configure deployment from GitHub for the slot
+$GitRepoUrl="https://github.com/karunakar1008/BlazorBlackBelt.git"
+$GitBranch="master"
+
+$PropertiesObject = @{
+    repoUrl = $GitRepoUrl;
+    branch =$GitBranch;
+    isManualIntegration = "true";
+}
+
+Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName $ResourceGroupName `
+-ResourceType Microsoft.Web/sites/slots/sourcecontrols `
+-ResourceName "$AppName/$SlotName/web" -ApiVersion 2015-08-01 -Force
+
+# Optional 
+
+# Restart the deployment slot to apply changes
+Restart-AzWebAppSlot -ResourceGroupName $ResourceGroupName -Name $AppName -Slot $SlotName
+Write-Output "Deployment Slot '$SlotName' deployed successfully. Access it at: https://$AppName-$SlotName.azurewebsites.net"
+
+
+# Output the URL of the deployed slot
+Write-Output "Deployment Slot URL: https://$AppName-$SlotName.azurewebsites.net"    
